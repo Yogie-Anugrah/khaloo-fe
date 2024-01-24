5d5bda6a6c2422c3c08aa5a0da991b0be03f567f
Lint Action <lint-action@samuelmeuli.com>
2024-01-24 12:53:48 +0000

    Fix code style issues with Prettier

--- app/(auth)/partner/login/page.tsx
+++ app/(auth)/partner/login/page.tsx
@@ -1,50 +1,50 @@
-import Input from "@/app/(auth)/text-field";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import Input from '@/app/(auth)/text-field';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 export default function SignUpPage() {
   return (
-    <main className="min-h-screen bg-white flex text-black font-poppins text-base xl:text-lg font-medium">
-      <div className="flex flex-col w-[3/5] flex-1 justify-center h-auto gap-14 px-8 sm:px-12 md:px-14 xl:gap-16 xl:px-20">
+    <main className='flex min-h-screen bg-white font-poppins text-base font-medium text-black xl:text-lg'>
+      <div className='flex h-auto w-[3/5] flex-1 flex-col justify-center gap-14 px-8 sm:px-12 md:px-14 xl:gap-16 xl:px-20'>
         {/* Title */}
-        <h1 className="max-md:text-center text-5xl xl:text-7xl font-extrabold">
+        <h1 className='text-5xl font-extrabold max-md:text-center xl:text-7xl'>
           Login
         </h1>
 
-        <form className="flex flex-col gap-7">
+        <form className='flex flex-col gap-7'>
           {/* Username */}
-          <Input type="text" variant="username" placeholder="Username" />
+          <Input type='text' variant='username' placeholder='Username' />
 
           {/* Password */}
-          <Input type="password" placeholder="Password" />
-          <div className="flex justify-between">
+          <Input type='password' placeholder='Password' />
+          <div className='flex justify-between'>
             {/* Container Checkbox */}
-            <div className="inline-flex gap-2 items-center xl:pl-6">
+            <div className='inline-flex items-center gap-2 xl:pl-6'>
               <input
-                type="checkbox"
-                id="remember"
-                name="remember"
-                className="border-[1px] border-[#323232] accent-[#898989] w-5 lg:w-7 aspect-square"
+                type='checkbox'
+                id='remember'
+                name='remember'
+                className='aspect-square w-5 border-[1px] border-[#323232] accent-[#898989] lg:w-7'
               />
-              <label htmlFor="remember">Remember me</label>
+              <label htmlFor='remember'>Remember me</label>
             </div>
             {/* Forgot Password */}
-            <Link href="/" className="hover:underline hover:underline-offset-4">
+            <Link href='/' className='hover:underline hover:underline-offset-4'>
               Forgot Password
             </Link>
             {/* Button Submit */}
           </div>
-          <button className="w-full font-bold rounded-lg bg-[#B2B2B2] text-white px-4 py-3 xl:px-4 xl:py-[18px]">
+          <button className='w-full rounded-lg bg-[#B2B2B2] px-4 py-3 font-bold text-white xl:px-4 xl:py-[18px]'>
             Submit
           </button>
         </form>
-        <p className="mx-auto h-fit">
-          {"Don't"} have an account?{" "}
-          <span className="text-[#888]">
+        <p className='mx-auto h-fit'>
+          {"Don't"} have an account?{' '}
+          <span className='text-[#888]'>
             <Link
-              href="/partner/sign-up"
-              className="hover:underline hover:underline-offset-4"
+              href='/partner/sign-up'
+              className='hover:underline hover:underline-offset-4'
             >
               Sign Up
             </Link>
@@ -52,9 +52,9 @@ export default function SignUpPage() {
         </p>
       </div>
       <Image
-        className="max-md:hidden md:w-[40vw] h-auto object-cover object-center grayscale-[85%]"
-        src="/assets/images/login.jpeg"
-        alt="Login Image"
+        className='h-auto object-cover object-center grayscale-[85%] max-md:hidden md:w-[40vw]'
+        src='/assets/images/login.jpeg'
+        alt='Login Image'
         width={790}
         height={1080}
       />
--- app/(auth)/partner/sign-up/page.tsx
+++ app/(auth)/partner/sign-up/page.tsx
@@ -1,69 +1,69 @@
-import Input from "@/app/(auth)/text-field";
-import Image from "next/image";
-import Link from "next/link";
+import Input from '@/app/(auth)/text-field';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function SignUpPartnerPage() {
   return (
-    <main className="min-h-screen bg-white flex text-black font-poppins text-base xl:text-lg font-medium">
-      <div className="flex flex-col w-[3/5] flex-1 justify-center h-auto gap-14 px-8 sm:px-12 md:px-14 xl:gap-16 xl:px-20">
+    <main className='flex min-h-screen bg-white font-poppins text-base font-medium text-black xl:text-lg'>
+      <div className='flex h-auto w-[3/5] flex-1 flex-col justify-center gap-14 px-8 sm:px-12 md:px-14 xl:gap-16 xl:px-20'>
         {/* Title */}
-        <h1 className="max-md:text-center text-5xl xl:text-7xl font-extrabold">
+        <h1 className='text-5xl font-extrabold max-md:text-center xl:text-7xl'>
           Sign Up
         </h1>
 
-        <form className="flex flex-col gap-7">
+        <form className='flex flex-col gap-7'>
           {/* email */}
-          <Input placeholder="Email / No.Hp" type="text" variant="username" />
+          <Input placeholder='Email / No.Hp' type='text' variant='username' />
           {/* Partner Category */}
-          <Input type="text" placeholder="Partner Category" />
-          <div className="flex flex-col gap-2 pl-4">
-            <div className="flex gap-2 lg:gap-4 items-center">
+          <Input type='text' placeholder='Partner Category' />
+          <div className='flex flex-col gap-2 pl-4'>
+            <div className='flex items-center gap-2 lg:gap-4'>
               <input
-                type="radio"
-                id="businesss"
-                name="category"
-                value="businesss"
-                className="border-[1px] border-[#323232] accent-[#898989] w-4 lg:w-7 aspect-square"
+                type='radio'
+                id='businesss'
+                name='category'
+                value='businesss'
+                className='aspect-square w-4 border-[1px] border-[#323232] accent-[#898989] lg:w-7'
               />
-              <label htmlFor="businesss">Business Partner</label>
+              <label htmlFor='businesss'>Business Partner</label>
             </div>
-            <div className="flex gap-2 lg:gap-4 items-center">
+            <div className='flex items-center gap-2 lg:gap-4'>
               <input
-                type="radio"
-                id="reseller"
-                name="category"
-                value="reseller"
-                className="border-[1px] border-[#323232] accent-[#898989] w-4 lg:w-7 aspect-square"
+                type='radio'
+                id='reseller'
+                name='category'
+                value='reseller'
+                className='aspect-square w-4 border-[1px] border-[#323232] accent-[#898989] lg:w-7'
               />
-              <label htmlFor="reseller">Reseller</label>
+              <label htmlFor='reseller'>Reseller</label>
             </div>
-            <div className="flex gap-2 lg:gap-4 items-center">
+            <div className='flex items-center gap-2 lg:gap-4'>
               <input
-                type="radio"
-                id="businesss"
-                name="category"
-                value="businesss"
-                className="border-[1px] border-[#323232] accent-[#898989] w-4 lg:w-7 aspect-square"
+                type='radio'
+                id='businesss'
+                name='category'
+                value='businesss'
+                className='aspect-square w-4 border-[1px] border-[#323232] accent-[#898989] lg:w-7'
               />
-              <label htmlFor="businesss">Affiliate</label>
+              <label htmlFor='businesss'>Affiliate</label>
             </div>
           </div>
-          <Input type="text" placeholder="XXXXXXXXXX" />
-          <Input type="text" placeholder="XXXXXXXXXX" />
+          <Input type='text' placeholder='XXXXXXXXXX' />
+          <Input type='text' placeholder='XXXXXXXXXX' />
           {/* Password */}
-          <Input type="password" placeholder="Password" />
+          <Input type='password' placeholder='Password' />
           {/* Confirm Pass */}
-          <Input type="password" placeholder="Confirm Password" />
-          <button className="w-full font-bold rounded-lg bg-[#B2B2B2] text-white px-4 py-3 xl:px-4 xl:py-[18px]">
+          <Input type='password' placeholder='Confirm Password' />
+          <button className='w-full rounded-lg bg-[#B2B2B2] px-4 py-3 font-bold text-white xl:px-4 xl:py-[18px]'>
             Submit
           </button>
         </form>
-        <p className="mx-auto h-fit">
-          Already have an account?{" "}
-          <span className="text-[#888]">
+        <p className='mx-auto h-fit'>
+          Already have an account?{' '}
+          <span className='text-[#888]'>
             <Link
-              href="/partner/login"
-              className="hover:underline hover:underline-offset-4"
+              href='/partner/login'
+              className='hover:underline hover:underline-offset-4'
             >
               Login
             </Link>
@@ -71,9 +71,9 @@ export default function SignUpPartnerPage() {
         </p>
       </div>
       <Image
-        className="max-md:hidden md:w-[40vw] h-auto object-cover object-center grayscale-[85%]"
-        src="/assets/images/login.jpeg"
-        alt="Login Image"
+        className='h-auto object-cover object-center grayscale-[85%] max-md:hidden md:w-[40vw]'
+        src='/assets/images/login.jpeg'
+        alt='Login Image'
         width={790}
         height={1080}
       />
