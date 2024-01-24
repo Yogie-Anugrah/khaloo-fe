9905a42f85359b6e00bddfba12b5e10b06e049f2
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2024-01-24 19:53:31 +0700

    fix: resolve merge conflict

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,27 +1,27 @@
-'use client';
+"use client";
 
-import clsx from 'clsx';
-import Image from 'next/image';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { type Dispatch, type SetStateAction, useEffect, useRef } from 'react';
+import clsx from "clsx";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { type Dispatch, type SetStateAction, useEffect, useRef } from "react";
 
 const navData = [
   {
-    name: 'HOME',
-    path: '/',
+    name: "HOME",
+    path: "/",
   },
   {
-    name: 'PRODUCT',
-    path: '/product',
+    name: "PRODUCT",
+    path: "/product",
   },
   {
-    name: 'EDU-SKIN',
-    path: '/edu-skin',
+    name: "EDU-SKIN",
+    path: "/edu-skin",
   },
   {
-    name: 'PARTNER WITH US',
-    path: '/partner-with-us',
+    name: "PARTNER WITH US",
+    path: "/partner-with-us",
   },
 ];
 
@@ -50,90 +50,90 @@ export default function Navbar({
       }
     }
     // Bind the event listener
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
       // Unbind the event listener on clean up
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [setNavbarExpanded]);
 
   return (
     <nav
-      aria-label='Navbar'
-      className='font-poppins sticky top-0 z-50 flex w-full flex-row items-center justify-between gap-4 overflow-hidden bg-white
-      px-4 py-4 text-black md:px-10 lg:py-5 xl:px-20'
+      aria-label="Navbar"
+      className="font-poppins sticky top-0 z-50 flex w-full flex-row items-center justify-between gap-4 overflow-hidden bg-white
+      px-4 py-4 text-black md:px-10 lg:py-5 xl:px-20"
     >
       {/* Logo */}
       <Link
-        href='/'
-        className='text-2xl font-bold'
-        aria-label='Home'
-        data-cy='navbar-logo'
+        href="/"
+        className="text-2xl font-bold"
+        aria-label="Home"
+        data-cy="navbar-logo"
       >
         <Image
-          alt='Logo Icon'
+          alt="Logo Icon"
           width={180}
           height={40}
-          className='h-8 w-28 object-contain object-center lg:h-10 lg:w-40'
-          src={'/assets/images/logo.png'}
+          className="h-8 w-28 object-contain object-center lg:h-10 lg:w-40"
+          src={"/assets/images/logo.png"}
           priority
-          sizes={'(max-width: 640px) 70px, 100px'}
+          sizes={"(max-width: 640px) 70px, 100px"}
         />
       </Link>
 
       {/* Display URL Navigation */}
       <div
-        data-cy='navbar-expanded'
+        data-cy="navbar-expanded"
         className={`fixed right-0 top-0 z-10 flex h-full transform flex-col gap-6
         border-l-2 border-gray-3 bg-white p-5 text-base transition-all duration-300 ease-in-out max-lg:w-[60%]
         lg:static lg:h-auto lg:w-auto lg:flex-1 lg:translate-x-0 lg:flex-row lg:items-center lg:justify-between lg:gap-12 lg:border-none lg:bg-transparent lg:p-0 lg:text-lg ${
-          navBarExpanded ? 'translate-x-0' : 'translate-x-full'
+          navBarExpanded ? "translate-x-0" : "translate-x-full"
         }`}
       >
-        <div className='flex flex-1 flex-col max-lg:gap-5 lg:flex-row '>
+        <div className="flex flex-1 flex-col max-lg:gap-5 lg:flex-row ">
           {/* Close Menu Mobile Button */}
           <button
-            className='ml-auto mr-4 lg:hidden'
+            className="ml-auto mr-4 lg:hidden"
             onClick={() => setNavbarExpanded(false)}
-            aria-label='Close Menu'
-            data-cy='navbar-close'
+            aria-label="Close Menu"
+            data-cy="navbar-close"
           >
             <Image
               height={20}
               width={20}
-              alt='close-button'
-              src='/assets/icons/x.svg'
+              alt="close-button"
+              src="/assets/icons/x.svg"
               className={clsx(
-                'transition-all duration-300',
-                navBarExpanded ? 'rotate-180' : 'rotate-0'
+                "transition-all duration-300",
+                navBarExpanded ? "rotate-180" : "rotate-0"
               )}
-              sizes='20px'
+              sizes="20px"
             />
           </button>
 
           {/* Map data navigation */}
-          <ul className='flex flex-1 flex-col items-center gap-5 font-medium lg:flex-row lg:justify-center lg:gap-12 xl:gap-28'>
+          <ul className="flex flex-1 flex-col items-center gap-5 font-medium lg:flex-row lg:justify-center lg:gap-12 xl:gap-28">
             {navData.map((item) => {
               return (
                 <li
                   key={item.name}
                   className={clsx(
-                    'transition-all duration-300 hover:underline hover:underline-offset-8 ',
+                    "transition-all duration-300 hover:underline hover:underline-offset-8 ",
                     pathname == item.path
-                      ? 'font-bold text-primary-2'
-                      : 'font-medium'
+                      ? "font-bold text-primary-2"
+                      : "font-medium"
                   )}
                 >
                   <Link
                     data-cy={`navbar-link-${item.name
                       .toLowerCase()
-                      .split(' ')
-                      .join('-')}`}
+                      .split(" ")
+                      .join("-")}`}
                     href={item.path}
                     aria-label={item.path
-                      .split('/')
+                      .split("/")
                       .map((item) => item)
-                      .join(' ')}
+                      .join(" ")}
                     data-navlink
                     onClick={() => setNavbarExpanded(false)}
                   >
@@ -146,35 +146,35 @@ export default function Navbar({
         </div>
       </div>
 
-      <div className='flex flex-shrink-0 items-center justify-center gap-3 font-medium'>
+      <div className="flex flex-shrink-0 items-center justify-center gap-3 font-medium">
         {/* Search and Person */}
-        <div className='flex items-center justify-center gap-2 sm:gap-3 lg:gap-6'>
+        <div className="flex items-center justify-center gap-2 sm:gap-3 lg:gap-6">
           <Image
             height={32}
             width={32}
-            src='/assets/icons/search.svg'
-            alt='Search Icon'
-            className='aspect-square w-7 sm:w-8'
-            sizes='(max-width: 640px) 28px, 32px'
+            src="/assets/icons/search.svg"
+            alt="Search Icon"
+            className="aspect-square w-7 sm:w-8"
+            sizes="(max-width: 640px) 28px, 32px"
           />
           <Image
             height={32}
             width={32}
-            src='/assets/icons/person.svg'
-            alt='Person Icon'
-            className='aspect-square w-7 sm:w-8'
-            sizes='(max-width: 640px) 28px, 32px'
+            src="/assets/icons/person.svg"
+            alt="Person Icon"
+            className="aspect-square w-7 sm:w-8"
+            sizes="(max-width: 640px) 28px, 32px"
           />
-          <button className='flex items-center gap-2'>
+          <button className="flex items-center gap-2">
             <Image
               height={24}
               width={24}
-              src='/assets/icons/browser.svg'
-              alt='Language Icon'
-              className='aspect-square w-6 sm:w-7'
-              sizes='(max-width: 640px) 24px, 32px'
+              src="/assets/icons/language.svg"
+              alt="Language Icon"
+              className="aspect-square w-6 sm:w-7"
+              sizes="(max-width: 640px) 24px, 32px"
             />
-            <p className='hidden hover:underline hover:underline-offset-4 lg:flex'>
+            <p className="hidden hover:underline hover:underline-offset-4 lg:flex">
               INA
             </p>
           </button>
@@ -182,17 +182,17 @@ export default function Navbar({
 
         {/* Hamburger Menu button Mobile */}
         <button
-          data-cy='navbar-toggle'
-          aria-label='Menu'
-          className='lg:hidden'
+          data-cy="navbar-toggle"
+          aria-label="Menu"
+          className="lg:hidden"
           onClick={() => setNavbarExpanded(true)}
         >
           <Image
             height={28}
             width={28}
-            src='/assets/icons/menu.svg'
-            alt='Menu Icon'
-            sizes='28px'
+            src="/assets/icons/menu.svg"
+            alt="Menu Icon"
+            sizes="28px"
           />
         </button>
       </div>
@@ -201,8 +201,8 @@ export default function Navbar({
       {navBarExpanded && (
         <div
           ref={sideBarBgRef}
-          data-cy='navbar-side-bar-bg'
-          className='fixed inset-0 z-0 h-full w-full bg-opacity-80 backdrop-blur-sm lg:hidden'
+          data-cy="navbar-side-bar-bg"
+          className="fixed inset-0 z-0 h-full w-full bg-opacity-80 backdrop-blur-sm lg:hidden"
         />
       )}
     </nav>
