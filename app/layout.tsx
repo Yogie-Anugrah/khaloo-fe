4a61618430a17b1e44407b76369b338725d693dd
Yogie-Anugrah <yogieanugrahramadhan@gmail.com>
2024-02-05 05:04:50 +0700

    add antd to dev

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -21,7 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className={poppins.className}>
-      <body>{children}</body>
+      <body>
+        <AntdRegistry>
+          {children}
+        </AntdRegistry>
+      </body>
     </html>
   );
 }
