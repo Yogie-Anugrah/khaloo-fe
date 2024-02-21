9ed21517af7633418e54efcf8a22039384fca181
Yogie-Anugrah <yogieanugrahramadhan@gmail.com>
2024-02-21 17:46:34 +0700

    FIX API CHANGES ON FRONT END
    TODO : ENDPOINT MUST BE ON VARIABLE

--- app/(site)/product/[id]/layout.tsx
+++ app/(site)/product/[id]/layout.tsx
@@ -2,7 +2,7 @@ import { type Metadata } from 'next'
 import { ReactNode } from 'react'
 
 export async function generateStaticParams() {
-  const productIds = await fetch('https://khaloo-be.vercel.app/products/list/id').then((res) => res.json())
+  const productIds = await fetch('https://khaloo-be-nine.vercel.app/products/list/id').then((res) => res.json())
   return productIds.map((prod: { prod_id: number }) => ({
     id: prod.prod_id.toString(),
   }))
@@ -21,7 +21,7 @@ export async function generateMetadata(
   const id = params.id
 
   // fetch data
-  const product = await fetch(`https://khaloo-be.vercel.app/products/metadata/${id}`).then((res) => res.json())
+  const product = await fetch(`https://khaloo-be-nine.vercel.app/products/metadata/${id}`).then((res) => res.json())
 
 
   return {
